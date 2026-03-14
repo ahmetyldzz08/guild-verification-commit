2ef1fb38e2f93b315a3136762adac01d80d49996
ahmetyldzz08 <ahmetyldzz08@outlook.com.tr>
2026-03-14 23:01:45 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T20:01:33.797Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-14T20:01:45.056Z
+Commit: Fix memory leak in event handlers
