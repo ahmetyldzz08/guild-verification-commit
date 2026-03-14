e156736425aefcaa7eec363204a1636b330f468a
ahmetyldzz08 <ahmetyldzz08@outlook.com.tr>
2026-03-14 23:05:19 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T20:01:42.830Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-14T20:05:19.059Z
+Commit: Fix memory leak in event handlers
