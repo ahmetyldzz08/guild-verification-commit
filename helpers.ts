61f673f0e8af37694bc5fedbf9dbd8f1f079596d
ahmetyldzz08 <ahmetyldzz08@outlook.com.tr>
2026-03-14 23:41:16 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T20:41:04.463Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-14T20:41:16.235Z
+Commit: Fix CSS styling bug on mobile devices
